317fe53ed0529d4d51f17d4c958b6644056b7ea1
Mike Shafland <michael.shafland@gmail.com>
2013-10-08 18:08:29 -0400

    Created a rails app and added statues

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'fab9431bb208e74201ec30b40d7167a98c682edc6e682c391244327b7a2f68be01e3aef9c223fa2a1fac239a3c4d65b7b8751764f9cd7336250de0b683ff53a5'
